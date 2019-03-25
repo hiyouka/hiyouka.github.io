6ef3da85738844e526d08d2809b3ebe0055d9b12
hiyouka <2062578173@qq.com>
2019-03-25 13:32:01 +0800

    Site updated: 2019-03-25 13:32:01

--- js/src/post-details.js
+++ js/src/post-details.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
 
   $(".post-body img").addClass("cover");
 
-  $(".cover").on('click','.cover', function(){
+  $(".cover").on('click', function(){
         console.log('666666666666666666');
         var this_ = $(this);
 	var images = this_.parents('.post-body').find('.cover');
